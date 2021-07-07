5eda4b6bd708620f161cf00b80264f7673f91fc1
Faav <52789876+withdrew@users.noreply.github.com>
2021-07-07 10:15:05 -0400

    Create precache-manifest.js

---
+++ precache-manifest.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce47fed5965eeae2bb12b22ce7eb5f1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "71ccc6b40de2bc30f7ee",
+    "url": "/portfolio/chunk.css"
+  },
+  {
+    "revision": "d5cacb0c235df2bfaa8e",
+    "url": "/portfolio/main.chunk.css"
+  },
+  {
+    "revision": "71ccc6b40de2bc30f7ee",
+    "url": "/profile/chunk.js"
+  },
+  {
+    "revision": "d5cacb0c235df2bfaa8e",
+    "url": "/portfolio/main.chunk.js"
+  }
+]);
