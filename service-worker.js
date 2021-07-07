31b54a4c1e4389581fac2e23891a304a8f84ec0d
Faav <52789876+withdrew@users.noreply.github.com>
2021-07-07 10:17:29 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-frontend-dev-portfolio/precache-manifest.js"
+  "/portfolio/precache-manifest.js"
 );
 
 self.addEventListener('message', (event) => {
